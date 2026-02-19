da9e4166bd709f95e6acacfc673ff83b06599070
SonnyHardy <htambeck@gmail.com>
2026-02-19 15:22:54 +0100

    Update application configuration and improve validation message for TodoRequest

--- src/main/java/com/sonny/todo/dto/TodoRequest.java
+++ src/main/java/com/sonny/todo/dto/TodoRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Size;
 
 public record TodoRequest(
         @NotBlank(message = "Title is required")
-        @Size(min = 3, max = 255, message = "Title must not exceed 255 characters")
+        @Size(min = 3, max = 255, message = "Title must have at least 3 and not exceed 255 characters")
         String title,
 
         String description,
